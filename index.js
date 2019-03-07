01a785de3737b8e6662108cb67104b1567b0a9a7
skymill2000 <skymill2000@gmail.com>
2019-03-07 15:46:14 +0900

    개인 정보 보기

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var app = express();
 var request = require('request');
 var path = require('path');
 var mysql = require('mysql');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'q1w2e3r4',
+  database : 'kisapay'
+}); 
+
 app.use(express.urlencoded());
 app.use(express.json());
 
@@ -13,6 +21,38 @@ app.use(express.static('public'));
  
 app.get('/index', function(req, res){
     res.render('home');
+})
+connection.connect();
+ 
+app.post('/join', function(req, res){
+    var name = req.body.name;
+    var password = req.body.password;
+    var id = req.body.id;
+    var accessToken = req.body.accessToken;
+    var refreshToken = req.body.refreshToken;
+    var useseqnum = req.body.useseqnum;
+    var sql = "INSERT INTO user (userid, userpassword, username, accessToken, refreshToken, useseqnum) VALUES (?,?,?,?,?,?)"
+    connection.query(sql,[id, password, name, accessToken, refreshToken, useseqnum ], function (error, results, fields) {
+        if (error) throw error;
+        else {
+            res.json(1)
+        }
+      });    
+})
+
+app.get('/user',function(req, res){
+    var accessToken = "927abc9b-b9d6-4e77-a651-fd10ee83e134";
+    var requestURL = "https://testapi.open-platform.or.kr/user/me?user_seq_no=1100034736";
+    var option = {
+        method : "GET",
+        url : requestURL,
+        headers : {
+            "Authorization" : "Bearer " + accessToken
+        }
+    }
+    request(option, function(err, response, body){
+        res.send(body);
+    })
 })
 
 app.get('/authResult', function(req, res){
