7f984cce1d15c1b9ee6abf4921e677598f3c031d
skymill2000 <skymill2000@gmail.com>
2019-03-07 13:42:36 +0900

    REQUESTLL

--- index.js
+++ index.js
@@ -15,6 +15,32 @@ app.get('/index', function(req, res){
     res.render('home');
 })
 
+app.get('/authResult', function(req, res){
+    var auth_code = req.query.code
+    var getTokenUrl = "https://testapi.open-platform.or.kr/oauth/2.0/token";
+    var option = {
+        method : "POST",
+        url :getTokenUrl,
+        headers : {
+            "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        form : {
+            code : auth_code,
+            client_id : "l7xx5f2432095adb49f9a8d05adf9dc09f9b",
+            client_secret : "d1480b0ce9484dd38b436f48ba1cfb42",
+            redirect_uri : "http://localhost:3000/authResult",
+            grant_type : "authorization_code"
+        }
+    };
+    request(option, function(err, res, body){
+        if(err) throw err;
+        else {
+            console.log(body);
+        }
+    })
+    console.log(auth_code);
+})
+
 app.get('/signup', function(req, res){
     res.render('signup');
 })
